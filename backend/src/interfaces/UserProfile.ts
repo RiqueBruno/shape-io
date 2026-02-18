4410995b0351abb49b26031cc4b4b384fc9266dc
Bruno Henrique <riquebruno@yahoo.com.br>
2026-02-18 20:43:49 -0300

    feat: add IUserProfile interface for user profile management

---
+++ backend/src/interfaces/UserProfile.ts
@@ -0,0 +1,11 @@
+export interface IUserProfile {
+  id: number;
+  name: string;
+  birth_date: string | Date;
+  sex: "M" | "F" | "O";
+  height: number;
+  start_weight: number;
+  goal_weight: number;
+  created_at: Date;
+  updated_at: Date;
+}
