13e8ad4cc348009ee848835c2e3f7efbb8bbc7df
Bruno Henrique <riquebruno@yahoo.com.br>
2026-02-20 22:16:53 -0300

    fix: update IUserProfile interface to correct user_id and make timestamps optional

--- backend/src/interfaces/UserProfile.ts
+++ backend/src/interfaces/UserProfile.ts
@@ -1,12 +1,12 @@
 //* Interface for creating a new user profile
 export interface IUserProfile {
-  id: number;
+  user_id: number;
   name: string;
   birth_date: string | Date;
   sex: "M" | "F" | "O";
   height: number;
   start_weight: number;
   goal_weight: number;
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
 }
